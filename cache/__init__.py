1fc573620b4d96243123d9f78d1327d4117f1bc0
Koisu <koisuspotify@gmail.com>
2025-05-09 17:03:25 -0700

    fixed error

--- cache/__init__.py
+++ cache/__init__.py
@@ -3,5 +3,5 @@ Contains caches and global constants
 """
 
 from cache.memory_cache import *
-from autocomplete import auto_complete_cache, auto_complete_cache_entry
+from cache.autocomplete import auto_complete_cache, auto_complete_cache_entry
 
